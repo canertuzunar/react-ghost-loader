5b347fd68becab7f6689008c00b1e4c333a01939
canertuzunar <176001027@stu.adu.edu.tr>
2021-03-11 18:15:21 +0300

    ADD: Component and Component's Style

---
+++ src/GhostLoader.js
@@ -0,0 +1,37 @@
+import React from'react'
+import splitAndConvert from '../utils/splitAndConvert'
+import './GhostLoader.css'
+const GhostLoader = ({width, height, row, widthRandom, heightRandom}) => {
+    const [w, widthType] = splitAndConvert(width)
+    const [h, heightType] = splitAndConvert(height)
+    const LoaderElements =[]
+    for(let i = 0; i < row; i++){
+        const width = `${w - (Math.random() * w * widthRandom)}${widthType}`
+        const height = `${h - (Math.random() * h * heightRandom)}${heightType}`
+        LoaderElements.push(
+            <span
+            className="ghost-loader"
+            key={i}
+            style={{width, height}} 
+            >
+                &zwnj;
+            </span>
+        )
+    }
+    return (
+        <>
+        {LoaderElements}
+        <p>deneme</p>
+        </>
+    )
+}
+
+GhostLoader.defaultProps ={
+    width: "200px",
+    height: "100%",
+    widthRandom: 0.30,
+    heightRandom: 0.25,
+    row: 1
+}
+
+export default GhostLoader
