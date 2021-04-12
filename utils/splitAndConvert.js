d3d1a3afea63d57eb4e75a9e6a5ca9b44bdf4062
canertuzunar <176001027@stu.adu.edu.tr>
2021-04-12 18:03:24 +0300

    check parameter and function outputs

--- utils/splitAndConvert.js
+++ utils/splitAndConvert.js
@@ -1,5 +1,14 @@
 function splitAndConverts(data) {
+    if(!data){
+        throw Error('Value cannot be undefined')
+    }
+
     const [value, type] = data.match(/(\d+)|(\D+$)/g)
+
+    if(!type || !value){
+        throw new Error('Value or Value Type cannot be undefined')
+    }
+
      const INTVALUE = parseFloat(value)
 
     return [INTVALUE, type]
